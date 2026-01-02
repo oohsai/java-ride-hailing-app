05e063b04e5ff2075a18b2dd84a27e1eae13c503
Karan Singh <karansingh5112002@gmail.com>
2026-01-02 14:39:51 +0530

    feat: implement domain layer (User and Ride entities) and update project documentation (#4)

---
+++ src/main/java/com/rideapp/model/Ride.java
@@ -0,0 +1,78 @@
+package com.rideapp.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "rides")
+public class Ride {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int ride_id;
+
+    private int rider_id;
+    private Integer driver_id;
+    private String pickup_location;
+    private String drop_location;
+    private String status;
+
+    private Double fare;
+
+    public Ride() {
+    }
+
+    public int getRide_id() {
+        return ride_id;
+    }
+
+    public void setRide_id(int ride_id) {
+        this.ride_id = ride_id;
+    }
+
+    public int getRider_id() {
+        return rider_id;
+    }
+
+    public void setRider_id(int rider_id) {
+        this.rider_id = rider_id;
+    }
+
+    public Integer getDriver_id() {
+        return driver_id;
+    }
+
+    public void setDriver_id(Integer driver_id) {
+        this.driver_id = driver_id;
+    }
+
+    public String getPickup_location() {
+        return pickup_location;
+    }
+
+    public void setPickup_location(String pickup_location) {
+        this.pickup_location = pickup_location;
+    }
+
+    public String getDrop_location() {
+        return drop_location;
+    }
+
+    public void setDrop_location(String drop_location) {
+        this.drop_location = drop_location;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Double getFare() {
+        return fare;
+    }
+
+    public void setFare(Double fare) {
+        this.fare = fare;
+    }
+}
